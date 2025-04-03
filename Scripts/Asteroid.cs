4e489e54aef99cad70a2e35f4b0992b2408c7763
jobeep <jonah.heinisch+edu@gmail.com>
2025-04-03 08:02:44 +0200

    Added Asteroids and AsteroidSpawner

---
+++ Scripts/Asteroid.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float yPos = 15F;
+    public float ySpeed = 10f;
+
+    private int xPos = 0;
+    public float translation = 5f;
+
+    void Awake()
+    {
+        xPos = Random.Range(-1, 2);
+    }
+
+    void Update()
+    {
+        yPos -= ySpeed * Time.deltaTime;
+
+        if (this.transform.position.y > -15)
+        {
+            this.transform.position = new Vector3(xPos * this.translation, yPos, 0f);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
